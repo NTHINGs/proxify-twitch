efcf1c72a45f8e8da4fe04f9dffd9f965bd52df1
Mauricio Martinez <n_othing@hotmail.com>
2018-04-17 15:04:33 -0500

    First

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var morgan = require('morgan');
+var twitchStream = require('twitch-get-stream')('afzhp0rofl3vcedb5lt1si7cdv9m50');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+app.use(morgan('dev'));
+app.set('view engine', 'ejs');
+
+app.listen(port, function() {
+    console.log('Running on localhost:'+port);
+});
+
+app.get('/:channel', function (req, res) {
+    twitchStream.raw(req.params.channel).then(function(streams) {
+        console.log(streams);
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+    // res.render('player', {stream});
+});
