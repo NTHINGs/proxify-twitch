91562937838bb998ec496e697222a3ece0c45032
Mauricio Martinez <n_othing@hotmail.com>
2018-04-18 10:52:21 -0500

    testing buffer

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var twitchStream = require('twitch-get-stream')('afzhp0rofl3vcedb5lt1si7cdv9m50');
 var path = require('path');
-
+var hls = require('hls-buffer');
+var buffer;
 var app = express();
 
 var port = process.env.PORT || 3000;
@@ -22,4 +23,18 @@ app.get('/:channel', function (req, res) {
     .catch(function(error){
         console.log(error);
     });
+});
+
+app.get('watch/:url', function (req, res) {
+    buffer = hls(req.params.url);
+    buffer.playlist(function (err, pl) {
+        res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
+        res.end(pl);
+    });
+});
+
+app.get('/', function (req, res) {
+    var stream = buffer.segment(req.url);
+    res.setHeader('Content-Type', 'video/mp2s');
+    stream.pipe(res);
 });
